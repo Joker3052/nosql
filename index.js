c95a846ea84865b0ac32357bed87b89fba7b2098
Joker3052 <phuonghoangthanh3052@gmail.com>
2023-11-09 17:24:02 +0700

    119

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options('*', cors())
 // Middleware
 app.use(express.json()); // Sử dụng express.json() để xử lý JSON
 app.use(morgan('tiny'));
-
+app.use(authJwt());
+app.use(errorHandler);
 //Routes
 const productsRoutes = require('./routers/products');
 const categorysRoutes = require('./routers/category');
+const usersRoutes = require('./routers/user');
 
 const api = process.env.API_URL;
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/category`, categorysRoutes);
+app.use(`${api}/users`, usersRoutes);
 // Kết nối với cơ sở dữ liệu
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
